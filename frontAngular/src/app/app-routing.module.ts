aadcef197f1fb251a59c60e849f4d25b07d18b7d
Yann Malaquin <yann.malaquin@gmail.com>
2021-12-04 17:50:10 +0100

    :lipstick: Ajout du template

--- frontAngular/src/app/app-routing.module.ts
+++ frontAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AnnuaireComponent} from "./annuaire/annuaire.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AnnuaireComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
