4806800c7a0c10ccc15451d2745d844fd8e6a720
Yann Malaquin <yann.malaquin@gmail.com>
2022-01-05 18:05:00 +0100

    :goal_net:

--- Annuaire/src/main/java/fr/yannm/annuaire/AnnuaireApplication.java
+++ Annuaire/src/main/java/fr/yannm/annuaire/AnnuaireApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
+import java.util.Map;
+
 @SpringBootApplication
 @EnableKafka
 public class AnnuaireApplication {
 
     public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        String kafka = env.get("BOOTSTRAP_SERVERS");
+        System.out.println("--------------------------------"+ kafka + "-------------------------------------------------------");
+
         SpringApplication.run(AnnuaireApplication.class, args);
     }
 
